f7711bcd6871c2f493e765e6790c54469d590ce2
Anish Agrawal <contact.agranish@gmail.com>
2022-04-03 20:33:00 +0545

    added parking lot status

--- src/main/java/com/parkinglot/carsinfo/domain/StatusParkingLot.java
+++ src/main/java/com/parkinglot/carsinfo/domain/StatusParkingLot.java
@@ -3,11 +3,26 @@ package com.parkinglot.carsinfo.domain;
 
 import com.parkinglot.carsinfo.config.ParkingLotConfig;
 import com.parkinglot.carsinfo.exception.InternalServerErrorException;
+import com.parkinglot.carsinfo.exception.NotFoundException;
 
 public class StatusParkingLot extends ParkingLotConfig {
 
     @Override
     public void implementCommand(String[] implementationCommand) throws InternalServerErrorException {
-
+        try {
+            if (!parkingLot.isEmpty() || parkingLot != null) {
+                System.out.println("Slot No.\tRegistration No.\tColor");
+                for (int i = 0; i <= parkingLot.size(); i++) {
+                    if (parkingLot.containsKey(i)) {
+                        Vehical vehical = parkingLot.get(i);
+                        System.out.println(i + "\t" + vehical.getRegistrationNumber() + "\t" + vehical.getColor());
+                    }
+                }
+            } else {
+                throw new NotFoundException("Parking Lot Not Found. Create one first");
+            }
+        } catch (Exception e) {
+            throw new InternalServerErrorException(e.getMessage(), e);
+        }
     }
 }
